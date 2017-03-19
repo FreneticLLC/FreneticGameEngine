245fb1fd02c18718bc9de9da3316268562825c4e
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2017-03-19 02:08:37 -0700

    Add a graphics helper

---
+++ FreneticGameGraphics/GraphicsUtil.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreneticGameGraphics
+{
+    /// <summary>
+    /// Helper class for graphical systems.
+    /// </summary>
+    public static class GraphicsUtil
+    {
+        /// <summary>
+        /// Checks errors when debug is enabled.
+        /// </summary>
+        /// <param name="loc">The source calling location.</param>
+        public static void CheckError(string loc)
+        {
+#if DEBUG
+            ErrorCode ec = GL.GetError();
+            while (ec != ErrorCode.NoError)
+            {
+                SysConsole.Output(OutputType.ERROR, "OpenGL error [" + loc + "]: " + ec + "\n" + Environment.StackTrace);
+                ec = GL.GetError();
+            }
+#endif
+        }
+
+    }
+}
