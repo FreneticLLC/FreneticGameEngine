7cbe42250810ad55a44054add300560dd5b27637
mcmonkey4eva <agoodwin@freneticllc.com>
2017-08-16 01:17:47 -0700

    Move in a copy of the Forward shaders

---
+++ FreneticGameGraphics/shaders/forward.fs
@@ -0,0 +1,293 @@
+#version 430 core
+
+#define MCM_TRANSP 0
+#define MCM_GEOM_ACTIVE 0
+#define MCM_NO_ALPHA_CAP 0
+#define MCM_BRIGHT 0
+#define MCM_INVERSE_FADE 0
+#define MCM_FADE_DEPTH 0
+#define MCM_LIGHTS 0
+#define MCM_SHADOWS 0
+#define MCM_TH 0
+#define MCM_SKY_FOG 0
+#define MCM_ANTI_TRANSP 0
+#define MCM_SIMPLE_LIGHT 0
+
+#if MCM_GEOM_ACTIVE
+layout (binding = 0) uniform sampler2DArray s;
+layout (binding = 1) uniform sampler2DArray normal_tex;
+#if MCM_LIGHTS
+layout (binding = 2) uniform sampler2DArray spec;
+#endif
+#else
+layout (binding = 0) uniform sampler2D s;
+layout (binding = 1) uniform sampler2D normal_tex;
+#if MCM_LIGHTS
+layout (binding = 2) uniform sampler2D spec;
+#endif
+#endif
+layout (binding = 4) uniform sampler2D depth;
+layout (binding = 5) uniform sampler2DArray shadowtex;
+// ...
+
+in struct vox_fout
+{
+	mat3 tbn;
+	vec3 pos;
+#if MCM_GEOM_ACTIVE
+	vec3 texcoord;
+#else
+	vec2 texcoord;
+#endif
+	vec4 color;
+#if MCM_INVERSE_FADE
+	float size;
+#endif
+#if MCM_FADE_DEPTH
+	float size;
+#endif
+} fi;
+
+const int LIGHTS_MAX = 38;
+
+// ...
+layout (location = 4) uniform vec4 screen_size = vec4(1024, 1024, 0.1, 1000.0);
+// ...
+layout (location = 6) uniform float time;
+// ...
+layout (location = 10) uniform vec3 sunlightDir = vec3(0.0, 0.0, -1.0);
+layout (location = 11) uniform vec3 maximum_light = vec3(0.9, 0.9, 0.9);
+layout (location = 12) uniform vec4 fogCol = vec4(0.0);
+layout (location = 13) uniform float fogDist = 1.0 / 100000.0;
+layout (location = 14) uniform vec2 zdist = vec2(1.0, 1000.0);
+layout (location = 15) uniform float lights_used = 0.0;
+layout (location = 16) uniform float minimum_light = 0.2;
+#if MCM_LIGHTS
+layout (location = 20) uniform mat4 shadow_matrix_array[LIGHTS_MAX];
+layout (location = 58) uniform mat4 light_data_array[LIGHTS_MAX];
+#endif
+
+layout (location = 0) out vec4 color;
+layout (location = 1) out vec4 position;
+layout (location = 2) out vec4 nrml;
+// ...
+layout (location = 4) out vec4 renderhint2;
+
+float snoise2(in vec3 v);
+
+vec4 unused_nonsense() // Prevent shader compiler from claiming variables are unused (Even if they /are/ unused!)
+{
+	return screen_size + fogCol + vec4(zdist, zdist);
+}
+
+float linearizeDepth(in float rinput) // Convert standard depth (stretched) to a linear distance (still from 0.0 to 1.0).
+{
+	return (2.0 * zdist.x) / (zdist.y + zdist.x - rinput * (zdist.y - zdist.x));
+}
+
+void applyFog()
+{
+#if MCM_SKY_FOG
+	float fmza = 1.0 - max(min((fi.pos.z - 1000.0) / 2000.0, 1.0), 0.0);
+	color.xyz = min(color.xyz * (1.0 - fmza) + fogCol.xyz * fmza, vec3(1.0));
+#endif
+#if MCM_BRIGHT
+	if (fogCol.w > 1.0)
+#endif
+	{
+		float dist = pow(dot(fi.pos, fi.pos) * fogDist, 0.6);
+		float fogMod = dist * exp(fogCol.w) * fogCol.w;
+		float fmz = min(fogMod, 1.0);
+		fmz *= fmz * fmz * fmz;
+		color.xyz = min(color.xyz * (1.0 - fmz) + fogCol.xyz * fmz, vec3(1.0));
+	}
+}
+
+float fix_sqr(in float inTemp)
+{
+	return 1.0 - (inTemp * inTemp);
+}
+
+#if MCM_GEOM_ACTIVE
+vec4 read_texture(in sampler2DArray samp_in, in vec3 texcrd)
+{
+	return texture(samp_in, texcrd);
+}
+#else
+vec4 read_texture(in sampler2D samp_in, in vec2 texcrd)
+{
+	return texture(samp_in, texcrd);
+}
+#endif
+
+void main()
+{
+	position = vec4(fi.pos, 1.0);
+	vec4 col = read_texture(s, fi.texcoord);
+#if MCM_LIGHTS
+	float specularStrength = read_texture(spec, fi.texcoord).x;
+#endif // MCM_LIGHTS
+#if MCM_NO_ALPHA_CAP
+	if (col.w * fi.color.w <= 0.01)
+	{
+		discard;
+	}
+#if MCM_ANTI_TRANSP
+	col.w = 1.0;
+#endif
+#else // MCM_NO_ALPHA_CAP
+#if MCM_TRANSP
+	if (col.w * fi.color.w >= 0.99)
+	{
+		discard;
+	}
+#else // MCM_TRANSP
+	if (col.w * fi.color.w < 0.99)
+	{
+		discard;
+	}
+#endif // else - MCM_TRANPS
+#endif // ELSE - MCM_NO_ALPHA_CAP
+	color = col * fi.color;
+#if MCM_BRIGHT
+#else // MCM_BRIGHT
+	float opac_min = 0.0;
+	vec3 norms = read_texture(normal_tex, fi.texcoord).xyz * 2.0 - vec3(1.0);
+	vec3 tf_normal = normalize(fi.tbn * norms);
+	nrml = vec4(tf_normal, 1.0);
+#if MCM_LIGHTS
+	vec3 res_color = vec3(0.0);
+	int count = int(lights_used);
+	for (int i = 0; i < count; i++)
+	{
+		mat4 light_data = light_data_array[i];
+		mat4 shadow_matrix = shadow_matrix_array[i];
+		// Light data.
+		vec3 light_pos = vec3(light_data[0][0], light_data[0][1], light_data[0][2]); // The position of the light source.
+		float diffuse_albedo = light_data[0][3]; // The diffuse albedo of this light (diffuse light is multiplied directly by this).
+		float specular_albedo = light_data[1][0]; // The specular albedo (specular power is multiplied directly by this).
+		float should_sqrt = light_data[1][1]; // 0 to not use square-root trick, 1 to use it (see implementation for details).
+		vec3 light_color = vec3(light_data[1][2], light_data[1][3], light_data[2][0]); // The color of the light.
+		float light_radius = light_data[2][1]; // The maximum radius of the light.
+		vec3 eye_pos = vec3(light_data[2][2], light_data[2][3], light_data[3][0]); // The position of the camera eye.
+		float light_type = light_data[3][1]; // What type of light this is: 0 is standard (point, sky, etc.), 1 is conical (spot light).
+		int is_point = light_type >= 1.5 ? 1 : 0;
+		float tex_size = light_data[3][2]; // If shadows are enabled, this is the inverse of the texture size of the shadow map.
+		// float unused = light_data[3][3];
+		vec4 f_spos = is_point == 1 ? vec4(0.0, 0.0, 0.0, 1.0) : shadow_matrix * vec4(fi.pos, 1.0); // Calculate the position of the light relative to the view.
+		f_spos /= f_spos.w; // Standard perspective divide.
+		vec3 light_path = light_pos - fi.pos; // What path a light ray has to travel down in theory to get from the source to the current pixel.
+		float light_length = length(light_path); // How far the light is from this pixel.
+		float d = light_length / light_radius; // How far the pixel is from the end of the light.
+		float atten = clamp(1.0 - (d * d), 0.0, 1.0); // How weak the light is here, based purely on distance so far.
+		if (is_point == 0 && light_type >= 0.5) // If this is a conical (spot light)...
+		{
+			atten *= 1.0 - (f_spos.x * f_spos.x + f_spos.y * f_spos.y); // Weaken the light based on how far towards the edge of the cone/circle it is. Bright in the center, dark in the corners.
+		}
+		if (atten <= 0.0) // If light is really weak...
+		{
+			continue; // Forget this light, move on already!
+		}
+		if (should_sqrt >= 0.5) // If inverse square trick is enabled (generally this will be 1.0 or 0.0)
+		{
+			f_spos.x = sign(f_spos.x) * fix_sqr(1.0 - abs(f_spos.x)); // Inverse square the relative position while preserving the sign. Shadow creation buffer also did this.
+			f_spos.y = sign(f_spos.y) * fix_sqr(1.0 - abs(f_spos.y)); // This section means that coordinates near the center of the light view will have more pixels per area available than coordinates far from the center.
+		}
+#if MCM_SIMPLE_LIGHT
+		const float depth = 1.0;
+#else
+		vec3 fs = vec3(0.0);
+		if (is_point == 0)
+		{
+			// Create a variable representing the proper screen/texture coordinate of the shadow view (ranging from 0 to 1 instead of -1 to 1).
+			fs = f_spos.xyz * 0.5 + vec3(0.5, 0.5, 0.5); 
+			if (fs.x < 0.0 || fs.x > 1.0
+				|| fs.y < 0.0 || fs.y > 1.0
+				|| fs.z < 0.0 || fs.z > 1.0) // If any coordinate is outside view range...
+			{
+				continue; // We can't light it! Discard straight away!
+			}
+		}
+		// TODO: maybe HD well blurred shadows?
+#if MCM_SHADOWS
+		float shadowID = float(i);
+		float mdX = 1.0, mdY = 1.0, rdX = 0.0, rdY = 0.0;
+		if (i >= 10)
+		{
+			shadowID = float((i - 10) / 4);
+			int ltCO = (i - 10) % 4;
+			rdY = float(ltCO / 2) * 0.5;
+			rdX = float(ltCO % 2) * 0.5;
+			mdX = 0.5;
+			mdY = 0.5;
+		}
+#if 1 // TODO: MCM_SHADOW_BLURRING?
+		float depth = 1.0;
+		if (is_point == 0)
+		{
+			int loops = 0;
+			for (float x = -1.0; x <= 1.0; x += 0.5)
+			{
+				for (float y = -1.0; y <= 1.0; y += 0.5)
+				{
+					loops++;
+					float rd = texture(shadowtex, vec3((fs.x + x * tex_size) * mdX + rdX, (fs.y + y * tex_size) * mdY + rdY, shadowID)).r; // Calculate the depth of the pixel.
+					depth += (rd >= (fs.z - 0.001) ? 1.0 : 0.0);
+				}
+			}
+			depth /= loops;
+		}
+#else
+		float depth = 1.0;
+		if (is_point == 0)
+		{
+			float rd = texture(shadowtex, vec3(fs.x * mdX + rdX, fs.y * mdY + rdY, shadowID)).r; // Calculate the depth of the pixel.
+			depth = (rd >= (fs.z - 0.001) ? 1.0 : 0.0); // If we have a bad graphics card, just quickly get a 0 or 1 depth value. This will be pixelated (hard) shadows!
+		}
+#endif
+		if (depth <= 0.0)
+		{
+			continue;
+		}
+#else
+		const float depth = 1.0;
+#endif
+#endif
+		vec3 L = light_path / light_length; // Get the light's movement direction as a vector
+		vec3 diffuse = max(dot(tf_normal, L), 0.0) * vec3(diffuse_albedo); // Find out how much diffuse light to apply
+		vec3 reller = normalize(fi.pos - eye_pos);
+		float spec_res = pow(max(dot(reflect(L, -tf_normal), reller), 0.0), 200.0) * specular_albedo * specularStrength;
+		opac_min += spec_res;
+		vec3 specular = vec3(spec_res); // Find out how much specular light to apply.
+		res_color += (vec3(depth, depth, depth) * atten * (diffuse * light_color) * color.xyz) + (min(specular, 1.0) * light_color * atten * depth); // Put it all together now.
+	}
+	color.xyz = min(res_color * (1.0 - max(0.2, minimum_light)) + color.xyz * max(0.2, minimum_light), vec3(1.0));
+#else // MCM_LIGHTS
+	float dotted = dot(-tf_normal, sunlightDir);
+	dotted = dotted <= 0.0 ? 0.0 : sqrt(dotted);
+	color.xyz *= min(max(dotted * maximum_light, max(0.2, minimum_light)), 1.0) * 0.75;
+#endif // else - MCM_LIGHTS
+	applyFog();
+#if MCM_TRANSP
+#endif // MCM_TRANSP
+#endif // else - MCM_BRIGHT
+	applyFog();
+#if MCM_INVERSE_FADE
+	float dist = linearizeDepth(gl_FragCoord.z);
+	vec2 fc_xy = gl_FragCoord.xy / screen_size.xy;
+	float depthval = linearizeDepth(texture(depth, fc_xy).x);
+	float mod2 = min(max(0.001 / max(depthval - dist, 0.001), 0.0), 1.0);
+	if (mod2 < 0.8)
+	{
+		discard;
+	}
+#endif // MCM_INVERSE_FADE
+#if MCM_FADE_DEPTH
+	float dist = linearizeDepth(gl_FragCoord.z);
+	vec2 fc_xy = gl_FragCoord.xy / screen_size.xy;
+	float depthval = linearizeDepth(texture(depth, fc_xy).x);
+	color.w *= min(max((depthval - dist) * fi.size * 0.5 * (screen_size.w - screen_size.z), 0.0), 1.0);
+#endif // MCM_FADE_DEPTH
+}
+
+#include glnoise.inc
---
+++ FreneticGameGraphics/shaders/forward.vs
@@ -0,0 +1,169 @@
+#version 430 core
+
+#define MCM_GEOM_ACTIVE 0
+#define MCM_INVERSE_FADE 0
+#define MCM_NO_BONES 0
+#define MCM_TH 0
+#define MCM_GEOM_THREED_TEXTURE 0
+
+layout (location = 0) in vec4 position;
+layout (location = 1) in vec4 normal;
+layout (location = 2) in vec4 texcoords;
+layout (location = 3) in vec4 tangent;
+layout (location = 4) in vec4 color;
+#if MCM_GEOM_ACTIVE
+#else
+layout (location = 5) in vec4 Weights;
+layout (location = 6) in vec4 BoneID;
+layout (location = 7) in vec4 Weights2;
+layout (location = 8) in vec4 BoneID2;
+#endif
+
+#if MCM_GEOM_ACTIVE
+out struct vox_out
+#else
+out struct vox_fout
+#endif
+{
+	mat3 tbn;
+#if MCM_GEOM_ACTIVE
+#else
+	vec3 pos;
+#endif
+#if MCM_GEOM_THREED_TEXTURE
+	vec3 texcoord;
+#else
+	vec2 texcoord;
+#endif
+	vec4 color;
+#if MCM_GEOM_ACTIVE
+} f;
+#else
+} fi;
+#endif
+
+const int MAX_BONES = 200;
+
+vec4 color_for(in vec4 pos, in vec4 colt);
+float snoise2(in vec3 v);
+
+#if MCM_GEOM_ACTIVE
+#else
+layout (location = 1) uniform mat4 proj_matrix = mat4(1.0);
+#endif
+layout (location = 2) uniform mat4 mv_matrix = mat4(1.0);
+layout (location = 3) uniform vec4 v_color = vec4(1.0);
+// ...
+layout (location = 6) uniform float time;
+// ...
+#if MCM_GEOM_ACTIVE
+#else
+layout (location = 100) uniform mat4 simplebone_matrix = mat4(1.0);
+layout (location = 101) uniform mat4 boneTrans[MAX_BONES];
+#endif
+
+void main()
+{
+	mat4 mv_mat_simple = mv_matrix;
+	mv_mat_simple[3][0] = 0.0;
+	mv_mat_simple[3][1] = 0.0;
+	mv_mat_simple[3][2] = 0.0;
+#if MCM_GEOM_ACTIVE
+#if MCM_GEOM_THREED_TEXTURE
+	f.texcoord = texcoords.xyz;
+#else
+	f.texcoord = texcoords.xy;
+#endif
+	vec4 normo = mv_mat_simple * vec4(normal.xyz, 1.0);
+	f.tbn = transpose(mat3(vec3(0.0), vec3(0.0), normo.xyz)); // TODO: Improve for decals?!
+	f.color = color * v_color;
+	gl_Position = mv_matrix * vec4(position.xyz, 1.0);
+#else // MCM_GEOM_ACTIVE
+	vec4 pos1;
+	vec4 norm1;
+	fi.texcoord = texcoords.xy;
+#if MCM_NO_BONES
+	const float rem = 0.0;
+#else
+	float rem = Weights[0] + Weights[1] + Weights[2] + Weights[3] + Weights2[0] + Weights2[1] + Weights2[2] + Weights2[3];
+#endif
+	if (rem > 0.01)
+	{
+		mat4 BT = mat4(1.0);
+		BT = boneTrans[int(BoneID[0])] * Weights[0];
+		BT += boneTrans[int(BoneID[1])] * Weights[1];
+		BT += boneTrans[int(BoneID[2])] * Weights[2];
+		BT += boneTrans[int(BoneID[3])] * Weights[3];
+		BT += boneTrans[int(BoneID2[0])] * Weights2[0];
+		BT += boneTrans[int(BoneID2[1])] * Weights2[1];
+		BT += boneTrans[int(BoneID2[2])] * Weights2[2];
+		BT += boneTrans[int(BoneID2[3])] * Weights2[3];
+		BT += mat4(1.0) * (1.0 - rem);
+		pos1 = vec4(position.xyz, 1.0) * BT;
+		norm1 = vec4(normal.xyz, 1.0) * BT;
+	}
+	else
+	{
+		pos1 = vec4(position.xyz, 1.0);
+		norm1 = vec4(normal.xyz, 1.0);
+	}
+	pos1 *= simplebone_matrix;
+	norm1 *= simplebone_matrix;
+	vec4 fnorm = mv_mat_simple * norm1;
+    fi.color = color_for(mv_matrix * vec4(pos1.xyz, 1.0), color * v_color);
+	vec4 posser = mv_matrix * vec4(pos1.xyz, 1.0);
+	fi.pos = posser.xyz;
+	gl_Position = proj_matrix * posser;
+	vec3 tf_normal = (mv_mat_simple * vec4(norm1.xyz, 0.0)).xyz; // TODO: Should BT be here?
+	vec3 tf_tangent = (mv_mat_simple * vec4(tangent.xyz, 0.0)).xyz; // TODO: Should BT be here?
+	vec3 tf_bitangent = (mv_mat_simple * vec4(cross(tangent.xyz, norm1.xyz), 0.0)).xyz; // TODO: Should BT be here?
+	fi.tbn = transpose(mat3(tf_tangent, tf_bitangent, tf_normal)); // TODO: Neccessity of transpose()?
+#endif // else - MCM_GEOM_ACTIVE
+}
+
+const float min_cstrobe = 3.0 / 255.0;
+
+const float min_transp = 1.0 / 255.0;
+
+vec4 color_for(in vec4 pos, in vec4 colt)
+{
+	if (colt.w <= min_transp)
+	{
+		if (colt.x == 0.0 && colt.y == 0.0 && colt.z == 0.0)
+		{
+			float r = snoise2(vec3((pos.x + time) * 0.1, (pos.y + time) * 0.1, (pos.z + time) * 0.1));
+			float g = snoise2(vec3((pos.x + 50.0 + time * 2) * 0.1, (pos.y + 127.0 + time * 1.7) * 0.1, (pos.z + 10.0 + time * 2.3) * 0.1));
+			float b = snoise2(vec3((pos.x - 50.0 - time) * 0.1, (pos.y - 65.0 - time * 1.56) * 0.1, (pos.z + 73.0 - time * 1.3) * 0.1));
+			return vec4(r, g, b, 1.0);
+		}
+		else if (colt.y < 0.7 && colt.y > 0.3 && colt.x == 0.0 && colt.z == 0.0)
+		{
+			return colt;
+		}
+		else if (colt.x > 0.3 && colt.x < 0.7 && colt.y > 0.3 && colt.y < 0.7 && colt.z > 0.3 && colt.z < 0.7)
+		{
+			return colt;
+		}
+		else
+		{
+			float adjust = abs(mod(time * 0.2, 2.0));
+			if (adjust > 1.0)
+			{
+				adjust = 2.0 - adjust;
+			}
+			return vec4(colt.x * adjust, colt.y * adjust, colt.z * adjust, 1.0);
+		}
+	}
+	else if (colt.w <= min_cstrobe)
+	{
+			float adjust = abs(mod(time * 0.2, 2.0));
+			if (adjust > 1.0)
+			{
+				adjust = 2.0 - adjust;
+			}
+			return vec4(1.0 - colt.x * adjust, 1.0 - colt.y * adjust, 1.0 - colt.z * adjust, 1.0);
+	}
+	return colt;
+}
+
+#include glnoise.inc
