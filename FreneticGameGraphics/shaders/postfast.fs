62ebfa9c84a38e877890c8e1a516cda963e0eee8
mcmonkey4eva <agoodwin@freneticllc.com>
2017-08-28 21:03:12 -0700

    License header!

--- FreneticGameGraphics/shaders/postfast.fs
+++ FreneticGameGraphics/shaders/postfast.fs
@@ -1,3 +1,11 @@
+//
+// This file is created by Frenetic LLC.
+// This code is Copyright (C) 2017 Frenetic LLC under the terms of a strict license.
+// See README.md or LICENSE.txt in the source root for the contents of the license.
+// If neither of these are available, assume that neither you nor anyone other than the copyright holder
+// hold any right or permission to use this software until such time as the official license is identified.
+//
+
 #version 430 core
 
 layout (binding = 0) uniform sampler2D rht;
