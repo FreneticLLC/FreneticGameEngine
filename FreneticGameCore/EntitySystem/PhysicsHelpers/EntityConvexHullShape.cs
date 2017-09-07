42a035dd760b936b1e9fabc7237d9ae1afdf7a75
Morphan1 <morphanone@gmail.com>
2017-09-07 02:31:01 -0400

    Add convex hull shape

---
+++ FreneticGameCore/EntitySystem/PhysicsHelpers/EntityConvexHullShape.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BEPUphysics.CollisionShapes;
+using BEPUphysics.CollisionShapes.ConvexShapes;
+
+namespace FreneticGameCore.EntitySystem.PhysicsHelpers
+{
+    /// <summary>
+    /// A convex hull shape for an entity.
+    /// </summary>
+    public class EntityConvexHullShape : EntityShapeHelper
+    {
+        /// <summary>
+        /// The internal convex hull shape.
+        /// </summary>
+        public ConvexHullShape Internal;
+
+        /// <summary>
+        /// Gets the BEPU shape object.
+        /// </summary>
+        /// <returns>The BEPU shape.</returns>
+        public override EntityShape GetBEPUShape()
+        {
+            return Internal;
+        }
+
+        /// <summary>
+        /// The string form of this shape helper.
+        /// </summary>
+        /// <returns>String form.</returns>
+        public override string ToString()
+        {
+            return "ConvexHullShape";
+        }
+    }
+}
