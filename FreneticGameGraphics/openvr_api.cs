2ec4c17d84f4ea22ecfc311c28a25e18124c4338
mcmonkey <agoodwin@freneticllc.com>
2019-01-22 22:04:57 -0800

    Update/add license header to all files - now without year for simplicity

--- FreneticGameGraphics/openvr_api.cs
+++ FreneticGameGraphics/openvr_api.cs
@@ -1,7 +1,7 @@
 //
-// This file is part of the game Voxalia, created by Frenetic LLC.
-// This code is Copyright (C) 2016-2017 Frenetic LLC under the terms of a strict license.
-// See README.md or LICENSE.txt in the source root for the contents of the license.
+// This file is part of the Frenetic Game Engine, created by Frenetic LLC.
+// This code is Copyright (C) Frenetic LLC under the terms of a strict license.
+// See README.md or LICENSE.txt in the FreneticGameEngine source root for the contents of the license.
 // If neither of these are available, assume that neither you nor anyone other than the copyright holder
 // hold any right or permission to use this software until such time as the official license is identified.
 //
