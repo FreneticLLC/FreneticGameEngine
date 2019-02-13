9df651200e8d95aeb2cd5338cdeb8ce973cf1204
mcmonkey <agoodwin@freneticllc.com>
2019-02-12 17:51:34 -0800

    rename 3: clear names

--- FreneticGameGraphics/ClientSystem/GE3DShaders.cs
+++ FreneticGameGraphics/ClientSystem/GE3DShaders.cs
@@ -53,10 +53,10 @@ namespace FreneticGameGraphics.ClientSystem
             string forw_extra = (forNorm ? ",MCM_NORMALS" : "")
                 + (forLight ? ",MCM_LIGHTS" : "")
                 + (forShad ? ",MCM_SHADOWS" : "");
-            Forward.s_forw = Shaders.GetShader("forward" + def + forw_extra);
-            Forward.s_forw_nobones = Shaders.GetShader("forward" + def + ",MCM_NO_BONES" + forw_extra);
-            Forward.s_forw_trans = Shaders.GetShader("forward" + def + ",MCM_TRANSP" + forw_extra);
-            Forward.s_forw_trans_nobones = Shaders.GetShader("forward" + def + ",MCM_TRANSP,MCM_NO_BONES" + forw_extra);
+            Forward.BasicSolid = Shaders.GetShader("forward" + def + forw_extra);
+            Forward.BasicSolid_NoBones = Shaders.GetShader("forward" + def + ",MCM_NO_BONES" + forw_extra);
+            Forward.BasicTransparent = Shaders.GetShader("forward" + def + ",MCM_TRANSP" + forw_extra);
+            Forward.BasicTransparent_NoBones = Shaders.GetShader("forward" + def + ",MCM_TRANSP,MCM_NO_BONES" + forw_extra);
             if (AllowLL)
             {
                 Deferred.s_transponly_ll = Shaders.GetShader("transponly" + def + ",MCM_LL");
@@ -66,17 +66,17 @@ namespace FreneticGameGraphics.ClientSystem
                 Deferred.s_ll_fpass = Shaders.GetShader("fpass" + def);
             }
             Deferred.s_hdrpass = Shaders.GetShader("hdrpass" + def);
-            Forward.s_post_fast = Shaders.GetShader("postfast" + def);
-            Forward.s_forw_grass = Shaders.GetShader("forward" + def + ",MCM_GEOM_ACTIVE,MCM_GEOM_THREED_TEXTURE" + forw_extra + "?grass");
-            Forward.s_fbo_grass = Shaders.GetShader("fbo" + def + ",MCM_GEOM_ACTIVE,MCM_PRETTY,MCM_GEOM_THREED_TEXTURE?grass");
+            Forward.PostProcess = Shaders.GetShader("postfast" + def);
+            Forward.Grass = Shaders.GetShader("forward" + def + ",MCM_GEOM_ACTIVE,MCM_GEOM_THREED_TEXTURE" + forw_extra + "?grass");
+            Deferred.s_fbo_grass = Shaders.GetShader("fbo" + def + ",MCM_GEOM_ACTIVE,MCM_PRETTY,MCM_GEOM_THREED_TEXTURE?grass");
             Deferred.s_shadow_grass = Shaders.GetShader("shadow" + def + ",MCM_GEOM_ACTIVE,MCM_PRETTY,MCM_SHADOWS,MCM_IS_A_SHADOW,MCM_GEOM_THREED_TEXTURE?grass");
             Deferred.s_shadow_parts = Shaders.GetShader("shadow" + def + ",MCM_GEOM_ACTIVE,MCM_PRETTY,MCM_SHADOWS,MCM_NO_ALPHA_CAP,MCM_FADE_DEPTH,MCM_IS_A_SHADOW?particles");
-            Forward.s_forw_particles = Shaders.GetShader("forward" + def + ",MCM_GEOM_ACTIVE,MCM_TRANSP,MCM_BRIGHT,MCM_NO_ALPHA_CAP,MCM_FADE_DEPTH" + forw_extra + "?particles");
+            Forward.Particles = Shaders.GetShader("forward" + def + ",MCM_GEOM_ACTIVE,MCM_TRANSP,MCM_BRIGHT,MCM_NO_ALPHA_CAP,MCM_FADE_DEPTH" + forw_extra + "?particles");
             Deferred.s_fbodecal = Shaders.GetShader("fbo" + def + ",MCM_INVERSE_FADE,MCM_NO_ALPHA_CAP,MCM_GEOM_ACTIVE,MCM_PRETTY?decal");
-            Forward.s_forwdecal = Shaders.GetShader("forward" + def + ",MCM_INVERSE_FADE,MCM_NO_ALPHA_CAP,MCM_GEOM_ACTIVE" + forw_extra + "?decal");
-            Forward.s_forwt_nofog = Shaders.GetShader("forward" + def + ",MCM_NO_ALPHA_CAP,MCM_BRIGHT,MCM_NO_BONES" + forw_extra);
-            Forward.s_forwt_obj = Shaders.GetShader("forward" + def + ",MCM_NO_BONES" + forw_extra);
-            Forward.s_forwt = Shaders.GetShader("forward" + def + ",MCM_NO_ALPHA_CAP,MCM_BRIGHT,MCM_NO_BONES,MCM_SKY_FOG" + forw_extra);
+            Forward.Decals = Shaders.GetShader("forward" + def + ",MCM_INVERSE_FADE,MCM_NO_ALPHA_CAP,MCM_GEOM_ACTIVE" + forw_extra + "?decal");
+            Forward.AllTransparencies_NoFog = Shaders.GetShader("forward" + def + ",MCM_NO_ALPHA_CAP,MCM_BRIGHT,MCM_NO_BONES" + forw_extra);
+            Forward.AllTransparencies_Objects = Shaders.GetShader("forward" + def + ",MCM_NO_BONES" + forw_extra);
+            Forward.AllTransparencies_Sky = Shaders.GetShader("forward" + def + ",MCM_NO_ALPHA_CAP,MCM_BRIGHT,MCM_NO_BONES,MCM_SKY_FOG" + forw_extra);
             Deferred.s_transponly_particles = Shaders.GetShader("transponly" + def + ",MCM_ANY,MCM_GEOM_ACTIVE,MCM_PRETTY,MCM_FADE_DEPTH?particles");
             Deferred.s_transponlylit_particles = Shaders.GetShader("transponly" + def + ",MCM_LIT,MCM_ANY,MCM_GEOM_ACTIVE,MCM_PRETTY,MCM_FADE_DEPTH?particles");
             Deferred.s_transponlylitsh_particles = Shaders.GetShader("transponly" + def + ",MCM_LIT,MCM_SHADOWS,MCM_ANY,MCM_GEOM_ACTIVE,MCM_PRETTY,MCM_FADE_DEPTH?particles");
@@ -96,62 +96,57 @@ namespace FreneticGameGraphics.ClientSystem
             /// <summary>
             /// The shader used for forward ('fast') rendering of data.
             /// </summary>
-            public Shader s_forw;
+            public Shader BasicSolid;
 
             /// <summary>
             /// The shader used for forward ('fast') rendering of data, with no bones.
             /// </summary>
-            public Shader s_forw_nobones;
+            public Shader BasicSolid_NoBones;
 
             /// <summary>
             /// The shader used for forward ('fast') rendering of transparent data.
             /// </summary>
-            public Shader s_forw_trans;
+            public Shader BasicTransparent;
 
             /// <summary>
             /// The shader for forward ('fast') post-processing calculations.
             /// </summary>
-            public Shader s_post_fast;
+            public Shader PostProcess;
 
             /// <summary>
             /// The shader used for forward ('fast') rendering of transparent data, with no bones.
             /// </summary>
-            public Shader s_forw_trans_nobones;
+            public Shader BasicTransparent_NoBones;
 
             /// <summary>
             /// The shader used for grass-sprites in forward rendering mode.
             /// </summary>
-            public Shader s_forw_grass;
-
-            /// <summary>
-            /// The shader used for grass-sprites in deferred rendering mode.
-            /// </summary>
-            public Shader s_fbo_grass;
+            public Shader Grass;
 
             /// <summary>
             /// The shader used for particles in forward rendering mode.
             /// </summary>
-            public Shader s_forw_particles;
+            public Shader Particles;
 
             /// <summary>
             /// The shader used for decal rendering in forward mode.
             /// </summary>
-            public Shader s_forwdecal;
+            public Shader Decals;
 
             /// <summary>
             /// The shader used for all-transparency rendering in forward mode (specifically not the skybox itself).
             /// </summary>
-            public Shader s_forwt_obj;
+            public Shader AllTransparencies_Objects;
 
             /// <summary>
             /// The shader used for all-transparency rendering in forward mode (specifically those objects that shouldn't have fog).
             /// </summary>
-            public Shader s_forwt_nofog;
+            public Shader AllTransparencies_NoFog;
 
             /// <summary>
             /// The shader used for all-transparency rendering in forward mode (primarily the skybox).
             /// </summary>
-            public Shader s_forwt;
+            public Shader AllTransparencies_Sky;
         }
 
         /// <summary>
@@ -218,6 +213,11 @@ namespace FreneticGameGraphics.ClientSystem
             /// The G-Buffer FBO shader, for alltransparents (Skybox mainly).
             /// </summary>
             public Shader s_fbot;
+
+            /// <summary>
+            /// The shader used for grass-sprites in deferred rendering mode.
+            /// </summary>
+            public Shader s_fbo_grass;
 
             /// <summary>
             /// The G-Buffer FBO shader, for the refraction pass.
