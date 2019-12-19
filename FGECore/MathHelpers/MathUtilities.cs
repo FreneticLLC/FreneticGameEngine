d91df7b5368e83b16f974786b68cd65892ff0935
Alex 'mcmonkey' Goodwin <agoodwin@freneticllc.com>
2019-12-18 19:17:30 -0800

    move some mathUtilities to FrenUtil's new MathHelper

--- FGECore/MathHelpers/MathUtilities.cs
+++ FGECore/MathHelpers/MathUtilities.cs
@@ -21,69 +21,6 @@ namespace FGECore.MathHelpers
     public static class MathUtilities
     {
         /// <summary>
-        /// Returns the next power of two.
-        /// Meaning, the next number in the sequence:
-        /// 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, ...
-        /// Result is >= input.
-        /// </summary>
-        /// <param name="x">The value, less than or equal to the result.</param>
-        /// <returns>The result, greater than or equal to the value.</returns>
-        public static int NextPowerOfTwo(int x)
-        {
-            Debug.Assert(x > 0, $"For NextPowerOfTwo, X must be > 0, but was {x}");
-            // Spread the Most Significant Bit all the way down
-            // so eg "00100100" becomes "11111100"
-            int spreadMSB = x | (x >> 1);
-            spreadMSB |= spreadMSB >> 2;
-            spreadMSB |= spreadMSB >> 4;
-            spreadMSB |= spreadMSB >> 8;
-            spreadMSB |= spreadMSB >> 16;
-            // Full value minus the downshift of it = *only* the MSB
-            int onlyMSB = spreadMSB - (spreadMSB >> 1);
-            // Exactly on MSB = return that, otherwise we're greater so grow by one.
-            if (x == onlyMSB)
-            {
-                return onlyMSB;
-            }
-            return onlyMSB << 1;
-        }
-
-        /// <summary>
-        /// Steps a value towards a goal by a specified amount, automatically moving the correct direction (positive or negative) and preventing going past the goal.
-        /// </summary>
-        /// <param name="start">The initial value.</param>
-        /// <param name="target">The goal value.</param>
-        /// <param name="amount">The amount to step by.</param>
-        /// <returns>The result.</returns>
-        public static double StepTowards(double start, double target, double amount)
-        {
-            if (start < target - amount)
-            {
-                return start + amount;
-            }
-            else if (start > target + amount)
-            {
-                return start - amount;
-            }
-            else
-            {
-                return target;
-            }
-        }
-
-        /// <summary>
-        /// Returns whether a number is close to another number, within a specified range.
-        /// </summary>
-        /// <param name="one">The first number.</param>
-        /// <param name="target">The second number.</param>
-        /// <param name="amount">The range.</param>
-        /// <returns>Whether it's close.</returns>
-        public static bool IsCloseTo(double one, double target, double amount)
-        {
-            return Math.Abs(one - target) < amount;
-        }
-
-        /// <summary>
         /// Represents the constant PI / 180.
         /// </summary>
         public const double PI180 = Math.PI / 180.0;
