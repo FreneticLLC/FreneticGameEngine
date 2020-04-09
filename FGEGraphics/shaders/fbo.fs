386ee9b53bffa37995ce577c28faea403f12fc56
Alex 'mcmonkey' Goodwin <agoodwin@freneticllc.com>
2020-04-09 13:01:56 -0700

    add a "write_hints" uniform to FBO fragment shader

--- FGEGraphics/shaders/fbo.fs
+++ FGEGraphics/shaders/fbo.fs
@@ -21,13 +21,13 @@ layout (binding = 2) uniform sampler2DArray spec;
 layout (binding = 3) uniform sampler2DArray refl;
 #if MCM_INVERSE_FADE
 layout (binding = 4) uniform sampler2D depth;
-#endif
-#else
+#endif // MCM_INVERSE_FADE
+#else // MCM_GEOM_ACTIVE
 layout (binding = 0) uniform sampler2D s;
 layout (binding = 1) uniform sampler2D normal_tex;
 layout (binding = 2) uniform sampler2D spec;
 layout (binding = 3) uniform sampler2D refl;
-#endif
+#endif // else - MCM_GEOM_ACTIVE
 
 // ...
 layout (location = 4) uniform vec4 screen_size = vec4(1024, 1024, 0.1, 1000.0);
@@ -35,20 +35,23 @@ layout (location = 4) uniform vec4 screen_size = vec4(1024, 1024, 0.1, 1000.0);
 layout (location = 9) uniform float refract_eta = 0.0;
 // ...
 layout (location = 16) uniform float minimum_light = 0.0;
+#if MCM_TRANSP_ALLOWED
+layout (location = 17) uniform float write_hints = 1.0;
+#endif // MCM_TRANSP_ALLOWED
 
 in struct vox_fout
 {
 	vec4 position;
 #if MCM_GEOM_ACTIVE
 	vec3 texcoord;
-#else
+#else // MCM_GEOM_ACTIVE
 	vec2 texcoord;
-#endif
+#endif // else - MCM_GEOM_ACTIVE
 	vec4 color;
 	mat3 tbn;
 #if MCM_INVERSE_FADE
 	float size;
-#endif
+#endif // MCM_INVERSE_FADE
 } fi;
 
 layout (location = 0) out vec4 color;
@@ -80,29 +83,42 @@ void main()
 	{
 		discard;
 	}
-#endif
+#endif // MCM_REFRACT
 #if MCM_NO_ALPHA_CAP
-#else
+#else // MCM_NO_ALPHA_CAP
 #if MCM_TRANSP_ALLOWED
 	if (col.w * fi.color.w < 0.01)
 	{
 		discard;
 	}
-#else
+#else // MCM_TRANSP_ALLOWED
 	if (col.w * fi.color.w < 0.99)
 	{
 		discard;
 	}
-#endif
-#endif
+#endif // else - MCM_TRANSP_ALLOWED
+#endif // else - MCM_NO_ALPHA_CAP
 	float specular_strength = texture(spec, fi.texcoord).r;
 	float reflection_amt = texture(refl, fi.texcoord).r;
 	vec3 norms = texture(normal_tex, fi.texcoord).xyz * 2.0 - vec3(1.0);
 	color = col * fi.color;
 	position = vec4(fi.position.xyz, 1.0);
 	normal = vec4(normalize(fi.tbn * norms), 1.0);
+#if MCM_TRANSP_ALLOWED
+	if (write_hints > 0.5)
+	{
+		renderhint = vec4(specular_strength, 0.0 /* TODO: ??? */, minimum_light, 1.0);
+		renderhint2 = vec4(0.0, reflection_amt, 0.0, 1.0);
+	}
+	else
+	{
+		renderhint = vec4(0.0);
+		renderhint2 = vec4(0.0);
+	}
+#else // MCM_TRANSP_ALLOWED
 	renderhint = vec4(specular_strength, 0.0 /* TODO: ??? */, minimum_light, 1.0);
 	renderhint2 = vec4(0.0, reflection_amt, 0.0, 1.0);
+#endif // else - MCM_TRANSP_ALLOWED
 #if MCM_INVERSE_FADE
 	float dist = linearizeDepth(gl_FragCoord.z);
 	vec2 fc_xy = gl_FragCoord.xy / screen_size.xy;
@@ -112,5 +128,5 @@ void main()
 	{
 		discard;
 	}
-#endif
+#endif // MCM_INVERSE_FADE
 }
