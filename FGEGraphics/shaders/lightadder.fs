3a67e9637a88d4a096ee8b11bda8d210320e1f48
Alex 'mcmonkey' Goodwin <agoodwin@freneticllc.com>
2020-05-11 17:57:59 -0700

    light adder tabs fix 2

--- FGEGraphics/shaders/lightadder.fs
+++ FGEGraphics/shaders/lightadder.fs
@@ -229,118 +229,118 @@ void main() // Let's put all code in main, why not...
 	int count = renderhint.z >= 1.0 ? 0 : int(lights_used);
 	for (int i = 0; i < count; i++)
 	{
-	mat4 light_data = light_data_array[i];
-	mat4 shadow_matrix = shadow_matrix_array[i];
-	// Light data.
-	vec3 light_pos = vec3(light_data[0][0], light_data[0][1], light_data[0][2]); // The position of the light source.
-	float diffuse_albedo = light_data[0][3]; // The diffuse albedo of this light (diffuse light is multiplied directly by this).
-	float specular_albedo = light_data[1][0]; // The specular albedo (specular power is multiplied directly by this).
-	float should_sqrt = light_data[1][1]; // 0 to not use square-root trick, 1 to use it (see implementation for details).
-	vec3 light_color = vec3(light_data[1][2], light_data[1][3], light_data[2][0]); // The color of the light.
-	float light_radius = light_data[2][1]; // The maximum radius of the light.
-	vec3 eye_pos = vec3(light_data[2][2], light_data[2][3], light_data[3][0]); // The position of the camera eye.
-	float light_type = light_data[3][1]; // What type of light this is: 0 is standard (point, sky, etc.), 1 is conical (spot light).
-	int is_point = light_type >= 1.5 ? 1 : 0;
-	float tex_size = light_data[3][2]; // If shadows are enabled, this is the inverse of the texture size of the shadow map.
-	// float unused = light_data[3][3];
-	vec4 f_spos = shadow_matrix * vec4(position, 1.0); // Calculate the position of the light relative to the view.
-	f_spos /= f_spos.w; // Standard perspective divide.
-	vec3 light_path = light_pos; // What path a light ray has to travel down in theory to get from the source to the current pixel.
-	float atten = 1.0;
-	float light_length = 1.0;
-	if (should_sqrt >= 0.5) // If inverse square trick is enabled (generally this will be 1.0 or 0.0)
-	{
-		f_spos.x = sign(f_spos.x) * fix_sqr(1.0 - abs(f_spos.x)); // Inverse square the relative position while preserving the sign. Shadow creation buffer also did this.
-		f_spos.y = sign(f_spos.y) * fix_sqr(1.0 - abs(f_spos.y)); // This section means that coordinates near the center of the light view will have more pixels per area available than coordinates far from the center.
-	}
-	else
-	{
-		light_path -= position;
-		light_length = length(light_path); // How far the light is from this pixel.
-		float d = light_length / light_radius; // How far the pixel is from the end of the light.
-		float atten = clamp(1.0 - (d * d), 0.0, 1.0); // How weak the light is here, based purely on distance so far.
-		if (is_point == 0 && light_type >= 0.5) // If this is a conical (spot light)...
+		mat4 light_data = light_data_array[i];
+		mat4 shadow_matrix = shadow_matrix_array[i];
+		// Light data.
+		vec3 light_pos = vec3(light_data[0][0], light_data[0][1], light_data[0][2]); // The position of the light source.
+		float diffuse_albedo = light_data[0][3]; // The diffuse albedo of this light (diffuse light is multiplied directly by this).
+		float specular_albedo = light_data[1][0]; // The specular albedo (specular power is multiplied directly by this).
+		float should_sqrt = light_data[1][1]; // 0 to not use square-root trick, 1 to use it (see implementation for details).
+		vec3 light_color = vec3(light_data[1][2], light_data[1][3], light_data[2][0]); // The color of the light.
+		float light_radius = light_data[2][1]; // The maximum radius of the light.
+		vec3 eye_pos = vec3(light_data[2][2], light_data[2][3], light_data[3][0]); // The position of the camera eye.
+		float light_type = light_data[3][1]; // What type of light this is: 0 is standard (point, sky, etc.), 1 is conical (spot light).
+		int is_point = light_type >= 1.5 ? 1 : 0;
+		float tex_size = light_data[3][2]; // If shadows are enabled, this is the inverse of the texture size of the shadow map.
+		// float unused = light_data[3][3];
+		vec4 f_spos = shadow_matrix * vec4(position, 1.0); // Calculate the position of the light relative to the view.
+		f_spos /= f_spos.w; // Standard perspective divide.
+		vec3 light_path = light_pos; // What path a light ray has to travel down in theory to get from the source to the current pixel.
+		float atten = 1.0;
+		float light_length = 1.0;
+		if (should_sqrt >= 0.5) // If inverse square trick is enabled (generally this will be 1.0 or 0.0)
 		{
-			atten *= 1.0 - (f_spos.x * f_spos.x + f_spos.y * f_spos.y); // Weaken the light based on how far towards the edge of the cone/circle it is. Bright in the center, dark in the corners.
+			f_spos.x = sign(f_spos.x) * fix_sqr(1.0 - abs(f_spos.x)); // Inverse square the relative position while preserving the sign. Shadow creation buffer also did this.
+			f_spos.y = sign(f_spos.y) * fix_sqr(1.0 - abs(f_spos.y)); // This section means that coordinates near the center of the light view will have more pixels per area available than coordinates far from the center.
 		}
-		if (atten <= 0.0) // If light is really weak...
+		else
 		{
-			continue; // Forget this light, move on already!
-		}
-	}
-#if MCM_SHADOWS
-	float depth = 1.0;
-#else // Shadows
-	const float depth = 1.0; // If shadows are off, depth is a constant 1.0!
-#endif // Else - Shadows
-	vec3 fs;
-	if (is_point == 0)
-	{
-		// Create a variable representing the proper screen/texture coordinate of the shadow view (ranging from 0 to 1 instead of -1 to 1).
-		fs = f_spos.xyz * 0.5 + vec3(0.5, 0.5, 0.5); 
-		if (fs.x < 0.0 || fs.x > 1.0
-			|| fs.y < 0.0 || fs.y > 1.0
-			|| fs.z < 0.0 || fs.z > 1.0) // If any coordinate is outside view range...
-		{
-			if (light_type >= 0.5)
+			light_path -= position;
+			light_length = length(light_path); // How far the light is from this pixel.
+			float d = light_length / light_radius; // How far the pixel is from the end of the light.
+			float atten = clamp(1.0 - (d * d), 0.0, 1.0); // How weak the light is here, based purely on distance so far.
+			if (is_point == 0 && light_type >= 0.5) // If this is a conical (spot light)...
 			{
-				continue;
+				atten *= 1.0 - (f_spos.x * f_spos.x + f_spos.y * f_spos.y); // Weaken the light based on how far towards the edge of the cone/circle it is. Bright in the center, dark in the corners.
+			}
+			if (atten <= 0.0) // If light is really weak...
+			{
+				continue; // Forget this light, move on already!
+			}
+		}
+#if MCM_SHADOWS
+		float depth = 1.0;
+#else // Shadows
+		const float depth = 1.0; // If shadows are off, depth is a constant 1.0!
+#endif // Else - Shadows
+		vec3 fs;
+		if (is_point == 0)
+		{
+			// Create a variable representing the proper screen/texture coordinate of the shadow view (ranging from 0 to 1 instead of -1 to 1).
+			fs = f_spos.xyz * 0.5 + vec3(0.5, 0.5, 0.5); 
+			if (fs.x < 0.0 || fs.x > 1.0
+				|| fs.y < 0.0 || fs.y > 1.0
+				|| fs.z < 0.0 || fs.z > 1.0) // If any coordinate is outside view range...
+			{
+				if (light_type >= 0.5)
+				{
+					continue;
+				}
+			}
+			else
+			{
+				// This block only runs if shadows are enabled.
+#if MCM_SHADOWS
+				depth = 0.0;
+				// Pretty quality (soft) shadows require a quality graphics card.
+#if MCM_GOOD_GRAPHICS
+				// This area is some calculus-ish stuff based upon NVidia sample code (naturally, it seems to run poorly on AMD cards. Good area to recode/optimize!)
+				// It's used to take the shadow map coordinates, and gets a safe Z-modifier value (See below).
+				vec3 duvdist_dx = dFdx(fs);
+				vec3 duvdist_dy = dFdy(fs);
+				vec2 dz_duv = vec2(duvdist_dy.y * duvdist_dx.z - duvdist_dx.y * duvdist_dy.z, duvdist_dx.x * duvdist_dy.z - duvdist_dy.x * duvdist_dx.z);
+				float tlen = (duvdist_dx.x * duvdist_dy.y) - (duvdist_dx.y * duvdist_dy.x);
+				dz_duv /= tlen;
+				float oneoverdj = 1.0 / depth_jump;
+				float jump = tex_size * depth_jump;
+				float depth_count = 0;
+				// Loop over an area quite near the pixel on the shadow map, but still covering multiple pixels of the shadow map.
+				for (float x = -oneoverdj * 2; x < oneoverdj * 2 + 1; x++)
+				{
+					for (float y = -oneoverdj * 2; y < oneoverdj * 2 + 1; y++)
+					{
+						float offz = dot(dz_duv, vec2(x * jump, y * jump)) * 1000.0; // Use the calculus magic from before to get a safe Z-modifier.
+						if (offz > -0.000001) // (Potentially removable?) It MUST be negative, and below a certain threshold. If it's not...
+						{
+							offz = -0.000001; // Force it to the threshold value to reduce errors.
+						}
+						//offz -= 0.001; // Set it a bit farther regardless to reduce bad shadows.
+						float rd = texture(shadowtex, vec3(fs.x + x * jump, fs.y + y * jump, float(i))).r; // Calculate the depth of the pixel.
+						depth += (rd >= (fs.z + offz) ? 1.0 : 0.0); // Get a 1 or 0 depth value for the current pixel. 0 means don't light, 1 means light.
+						depth_count++; // Can probably use math to generate this number rather than constantly incrementing a counter.
+					}
+				}
+				depth = depth / depth_count; // Average up the 0 and 1 light values to produce gray near the edges of shadows. Soft shadows, hooray!
+#else // Good Graphics
+				float rd = texture(shadowtex, vec3(fs.x, fs.y, float(i))).r; // Calculate the depth of the pixel.
+				float depth = (rd >= (fs.z - 0.001) ? 1.0 : 0.0); // If we have a bad graphics card, just quickly get a 0 or 1 depth value. This will be pixelated (hard) shadows!
+#endif // Else - Good Graphics
+				if (depth <= 0.0)
+				{
+					continue; // If we're a fully shadowed pixel, don't add any light!
+				}
+#endif // Shadows
 			}
 		}
 		else
 		{
-			// This block only runs if shadows are enabled.
-#if MCM_SHADOWS
-			depth = 0.0;
-			// Pretty quality (soft) shadows require a quality graphics card.
-#if MCM_GOOD_GRAPHICS
-			// This area is some calculus-ish stuff based upon NVidia sample code (naturally, it seems to run poorly on AMD cards. Good area to recode/optimize!)
-			// It's used to take the shadow map coordinates, and gets a safe Z-modifier value (See below).
-			vec3 duvdist_dx = dFdx(fs);
-			vec3 duvdist_dy = dFdy(fs);
-			vec2 dz_duv = vec2(duvdist_dy.y * duvdist_dx.z - duvdist_dx.y * duvdist_dy.z, duvdist_dx.x * duvdist_dy.z - duvdist_dy.x * duvdist_dx.z);
-			float tlen = (duvdist_dx.x * duvdist_dy.y) - (duvdist_dx.y * duvdist_dy.x);
-			dz_duv /= tlen;
-			float oneoverdj = 1.0 / depth_jump;
-			float jump = tex_size * depth_jump;
-			float depth_count = 0;
-			// Loop over an area quite near the pixel on the shadow map, but still covering multiple pixels of the shadow map.
-			for (float x = -oneoverdj * 2; x < oneoverdj * 2 + 1; x++)
-			{
-				for (float y = -oneoverdj * 2; y < oneoverdj * 2 + 1; y++)
-				{
-					float offz = dot(dz_duv, vec2(x * jump, y * jump)) * 1000.0; // Use the calculus magic from before to get a safe Z-modifier.
-					if (offz > -0.000001) // (Potentially removable?) It MUST be negative, and below a certain threshold. If it's not...
-					{
-						offz = -0.000001; // Force it to the threshold value to reduce errors.
-					}
-					//offz -= 0.001; // Set it a bit farther regardless to reduce bad shadows.
-					float rd = texture(shadowtex, vec3(fs.x + x * jump, fs.y + y * jump, float(i))).r; // Calculate the depth of the pixel.
-					depth += (rd >= (fs.z + offz) ? 1.0 : 0.0); // Get a 1 or 0 depth value for the current pixel. 0 means don't light, 1 means light.
-					depth_count++; // Can probably use math to generate this number rather than constantly incrementing a counter.
-				}
-			}
-			depth = depth / depth_count; // Average up the 0 and 1 light values to produce gray near the edges of shadows. Soft shadows, hooray!
-#else // Good Graphics
-			float rd = texture(shadowtex, vec3(fs.x, fs.y, float(i))).r; // Calculate the depth of the pixel.
-			float depth = (rd >= (fs.z - 0.001) ? 1.0 : 0.0); // If we have a bad graphics card, just quickly get a 0 or 1 depth value. This will be pixelated (hard) shadows!
-#endif // Else - Good Graphics
-			if (depth <= 0.0)
-			{
-				continue; // If we're a fully shadowed pixel, don't add any light!
-			}
-#endif // Shadows
+			fs = f_spos.xyz;
 		}
-	}
-	else
-	{
-		fs = f_spos.xyz;
-	}
-	vec3 L = light_path / light_length; // Get the light's movement direction as a vector
-	vec3 diffuse = max(dot(N, -L), 0.0) * vec3(diffuse_albedo) * HDR_Mod; // Find out how much diffuse light to apply
-	vec3 specular = vec3(pow(max(dot(reflect(L, N), normalize(position - eye_pos)), 0.0), 200.0) * specular_albedo * renderhint.x) * HDR_Mod; // Find out how much specular light to apply.
-	res_color += (vec3(depth, depth, depth) * atten * (diffuse * light_color) * diffuset.xyz) + (min(specular, 1.0) * light_color * atten * depth); // Put it all together now.
-	aff += atten;
+		vec3 L = light_path / light_length; // Get the light's movement direction as a vector
+		vec3 diffuse = max(dot(N, -L), 0.0) * vec3(diffuse_albedo) * HDR_Mod; // Find out how much diffuse light to apply
+		vec3 specular = vec3(pow(max(dot(reflect(L, N), normalize(position - eye_pos)), 0.0), 200.0) * specular_albedo * renderhint.x) * HDR_Mod; // Find out how much specular light to apply.
+		res_color += (vec3(depth, depth, depth) * atten * (diffuse * light_color) * diffuset.xyz) + (min(specular, 1.0) * light_color * atten * depth); // Put it all together now.
+		aff += atten;
 	}
 	res_color += (ambient + vec3(renderhint.z)) * HDR_Mod * diffuset.xyz; // Add ambient light.
 	color = vec4(res_color * ssao_mod, diffuset.w + aff + renderhint.z); // I don't know why this alpha value became necessary.
