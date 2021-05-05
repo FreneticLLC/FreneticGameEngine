c89989b462fea2387c55703cffdd81be1d806b8f
Alex 'mcmonkey' Goodwin <agoodwin@freneticllc.com>
2021-05-05 16:55:12 -0700

    file encoding fix and a few missing license headers

--- FGECore/EntitySystem/PhysicsHelpers/CollisionEvent.cs
+++ FGECore/EntitySystem/PhysicsHelpers/CollisionEvent.cs
@@ -1,4 +1,12 @@
-﻿using BepuPhysics.CollisionDetection;
+//
+// This file is part of the Frenetic Game Engine, created by Frenetic LLC.
+// This code is Copyright (C) Frenetic LLC under the terms of a strict license.
+// See README.md or LICENSE.txt in the FreneticGameEngine source root for the contents of the license.
+// If neither of these are available, assume that neither you nor anyone other than the copyright holder
+// hold any right or permission to use this software until such time as the official license is identified.
+//
+
+using BepuPhysics.CollisionDetection;
 using FGECore.MathHelpers;
 using System;
 using System.Collections.Generic;
