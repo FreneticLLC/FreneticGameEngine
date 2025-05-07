8d7be9f3d93391826f5056b4e524c0e2dcb44828
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2025-05-07 01:37:12 -0700

    hackfix for system drawing being async unsafe (??)

--- FGEGraphics/GraphicsHelpers/Textures/TextureEngine.cs
+++ FGEGraphics/GraphicsHelpers/Textures/TextureEngine.cs
@@ -101,6 +101,17 @@ public class TextureEngine : IDisposable
         LoadedTextures.Add("clear", Clear);
         NormalDef = GenerateForColor(Color.FromArgb(255, 127, 127, 255), "normal_def");
         LoadedTextures.Add("normal_def", NormalDef);
+        InternalFixLoad();
+    }
+
+    /// <summary>Experimental hack-fix: the underlying drawing code might be a bit sensitive to async initialization, so we intentionally poke it synchronously during the early init.</summary>
+    public void InternalFixLoad()
+    {
+        Bitmap bmp = White.SaveToBMP();
+        MemoryStream data = new();
+        bmp.Save(data, ImageFormat.Bmp);
+        data.Seek(0, SeekOrigin.Begin);
+        BitmapForBytes(data.ToArray());
     }
 
     /// <summary>Clears away all current textures.</summary>
