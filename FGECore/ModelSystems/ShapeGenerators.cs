0b541277e8bb96fa1f29e674d84a7cf239a28458
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2025-11-20 03:39:51 -0800

    Cube shape generator

--- FGECore/ModelSystems/ShapeGenerators.cs
+++ FGECore/ModelSystems/ShapeGenerators.cs
@@ -18,6 +18,126 @@ namespace FGECore.ModelSystems;
 /// <summary>Generates shapes and returns them as a model.</summary>
 public static class ShapeGenerators
 {
+    /// <summary>Generates a simple 3D cube model, centered at 0,0,0.</summary>
+    /// <param name="sideLength">Length of each side.</param>
+    public static Model3D GenerateCube(float sideLength)
+    {
+        // NOTE: This code is quite brute-forcey, but not sure if there's a more "proper" way to do it.
+        // 6 faces, 2 triangles per face, 3 indices per triangle.
+        Vector3[] vertices = new Vector3[6 * 2 * 3];
+        Vector3[] normals = new Vector3[6 * 2 * 3];
+        Vector2[] texCoords = new Vector2[6 * 2 * 3];
+        uint[] indices = new uint[6 * 2 * 3];
+        float halfSide = sideLength / 2;
+        // X+ 0..5
+        vertices[0] = new(halfSide, halfSide, halfSide);
+        vertices[1] = new(halfSide, halfSide, -halfSide);
+        vertices[2] = new(halfSide, -halfSide, -halfSide);
+        texCoords[0] = new(1, 0);
+        texCoords[1] = new(1, 1);
+        texCoords[2] = new(0, 1);
+        vertices[3] = new(halfSide, -halfSide, halfSide);
+        vertices[4] = new(halfSide, halfSide, halfSide);
+        vertices[5] = new(halfSide, -halfSide, -halfSide);
+        texCoords[3] = new(0, 0);
+        texCoords[4] = new(1, 0);
+        texCoords[5] = new(0, 1);
+        for (int i = 0; i < 6; i++)
+        {
+            normals[i] = new(1, 0, 0);
+        }
+        // Y+ 6..11
+        vertices[6] = new(halfSide, halfSide, halfSide);
+        vertices[7] = new(-halfSide, halfSide, halfSide);
+        vertices[8] = new(-halfSide, halfSide, -halfSide);
+        texCoords[6] = new(0, 0);
+        texCoords[7] = new(1, 0);
+        texCoords[8] = new(1, 1);
+        vertices[9] = new(halfSide, halfSide, -halfSide);
+        vertices[10] = new(halfSide, halfSide, halfSide);
+        vertices[11] = new(-halfSide, halfSide, -halfSide);
+        texCoords[9] = new(0, 1);
+        texCoords[10] = new(0, 0);
+        texCoords[11] = new(1, 1);
+        for (int i = 6; i < 12; i++)
+        {
+            normals[i] = new(0, 1, 0);
+        }
+        // Z+ 12..17
+        vertices[12] = new(-halfSide, -halfSide, halfSide);
+        vertices[13] = new(-halfSide, halfSide, halfSide);
+        vertices[14] = new(halfSide, halfSide, halfSide);
+        texCoords[12] = new(1, 0);
+        texCoords[13] = new(1, 1);
+        texCoords[14] = new(0, 1);
+        vertices[15] = new(-halfSide, -halfSide, halfSide);
+        vertices[16] = new(halfSide, halfSide, halfSide);
+        vertices[17] = new(halfSide, -halfSide, halfSide);
+        texCoords[15] = new(1, 0);
+        texCoords[16] = new(0, 1);
+        texCoords[17] = new(0, 0);
+        for (int i = 12; i < 18; i++)
+        {
+            normals[i] = new(0, 0, 1);
+        }
+        // X- 18..23
+        vertices[18] = new(-halfSide, -halfSide, -halfSide);
+        vertices[19] = new(-halfSide, halfSide, -halfSide);
+        vertices[20] = new(-halfSide, halfSide, halfSide);
+        texCoords[18] = new(1, 1);
+        texCoords[19] = new(0, 1);
+        texCoords[20] = new(0, 0);
+        vertices[21] = new(-halfSide, -halfSide, -halfSide);
+        vertices[22] = new(-halfSide, halfSide, halfSide);
+        vertices[23] = new(-halfSide, -halfSide, halfSide);
+        texCoords[21] = new(1, 1);
+        texCoords[22] = new(0, 0);
+        texCoords[23] = new(1, 0);
+        for (int i = 18; i < 24; i++)
+        {
+            normals[i] = new(-1, 0, 0);
+        }
+        // Y- 24..29
+        vertices[24] = new(-halfSide, -halfSide, -halfSide);
+        vertices[25] = new(-halfSide, -halfSide, halfSide);
+        vertices[26] = new(halfSide, -halfSide, halfSide);
+        texCoords[24] = new(0, 1);
+        texCoords[25] = new(0, 0);
+        texCoords[26] = new(1, 0);
+        vertices[27] = new(-halfSide, -halfSide, -halfSide);
+        vertices[28] = new(halfSide, -halfSide, halfSide);
+        vertices[29] = new(halfSide, -halfSide, -halfSide);
+        texCoords[27] = new(0, 1);
+        texCoords[28] = new(1, 0);
+        texCoords[29] = new(1, 1);
+        for (int i = 24; i < 30; i++)
+        {
+            normals[i] = new(0, -1, 0);
+        }
+        // Z- 30..35
+        vertices[30] = new(halfSide, halfSide, -halfSide);
+        vertices[31] = new(-halfSide, halfSide, -halfSide);
+        vertices[32] = new(-halfSide, -halfSide, -halfSide);
+        texCoords[30] = new(1, 1);
+        texCoords[31] = new(0, 1);
+        texCoords[32] = new(0, 0);
+        vertices[33] = new(halfSide, -halfSide, -halfSide);
+        vertices[34] = new(halfSide, halfSide, -halfSide);
+        vertices[35] = new(-halfSide, -halfSide, -halfSide);
+        texCoords[33] = new(1, 0);
+        texCoords[34] = new(1, 1);
+        texCoords[35] = new(0, 0);
+        for (int i = 30; i < 36; i++)
+        {
+            normals[i] = new(0, 0, -1);
+        }
+        for (uint i = 0; i < indices.Length; i++)
+        {
+            indices[i] = i;
+        }
+        return Internal.GetModelAfterGenerating("cube", vertices, normals, texCoords, indices);
+    }
+
     /// <summary>Generates a 3D UV-Sphere model, centered at 0,0,0. A UV-sphere is characterized by quad sidings, with triangles on top/bottom.
     /// This is type of sphere most people visualize, sometimes also known as a globe model.</summary>
     /// <param name="radius">Distance from center to an edge, standard round-object radius.</param>
