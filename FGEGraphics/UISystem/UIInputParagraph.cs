069dc6e7dab09d1c5d4272f60003a3764650b89f
Skye Prince <spiralixp@gmail.com>
2026-03-17 12:24:37 -0700

    document UIParagraph + UIInputParagraph

--- FGEGraphics/UISystem/UIInputParagraph.cs
+++ FGEGraphics/UISystem/UIInputParagraph.cs
@@ -20,20 +20,30 @@ using System.Threading.Tasks;
 
 namespace FGEGraphics.UISystem;
 
+/// <summary>Represents a <see cref="UIParagraph"/> designed to display the state of text during editing. Primarily used for <see cref="UIInputLabel"/>s</summary>
+/// <remarks>
+/// This paragraph contains three <see cref="UILabel"/>s. 
+/// The left and right labels display content on either side of the cursor position, while the center label specifically displays highlighted text.
+/// </remarks>
 public class UIInputParagraph : UIParagraph
 {
+    /// <inheritdoc/>
     public override string Name => "Input Paragraph";
 
+    /// <summary>The styling for text outside of a selection.</summary>
     public UIStyling TextStyling;
 
+    /// <summary>The styling for text inside of a selection.</summary>
     public UIStyling HighlightStyling;
 
+    /// <summary>Gets or sets the content of this paragraph.</summary>
+    /// <remarks>This operation calls <see cref="UpdateRenderState"/>.</remarks>
     public new string Content
     {
         get => InputInternal.Content;
         set
         {
-            InputInternal.Content = value ?? "";
+            SetContent(value);
             UpdateRenderState();
         }
     }
@@ -44,6 +54,7 @@ public class UIInputParagraph : UIParagraph
     /// <summary>The end cursor position.</summary>
     public int CursorEnd = 0;
 
+    /// <summary>Whether to render the cursor when no text is selected.</summary>
     public bool RenderCursor = false;
 
     /// <summary>The minimum cursor position.</summary>
@@ -52,26 +63,41 @@ public class UIInputParagraph : UIParagraph
     /// <summary>The maximum cursor position.</summary>
     public int CursorRight => CursorEnd > CursorStart ? CursorEnd : CursorStart;
 
-    /// <summary>Whether a string of text is selected between the indices.</summary>
+    /// <summary>Whether a non-empty string of text is selected between the cursor positions.</summary>
     public bool HasSelection => CursorStart != CursorEnd;
 
-    public struct InternalData()
+    // TODO: Selection?
+
+    /// <summary>Data internal to a <see cref="UIInputParagraph"/> instance.</summary>
+    public new struct InternalData()
     {
+        /// <summary>The paragraph text content.</summary>
         public string Content = "";
 
+        /// <summary>The label to the left of the cursor.</summary>
         public UILabel LabelLeft;
 
+        /// <summary>The label between the left and right cursor positions. This label is empty if both cursor positions are equal</summary>
         public UILabel LabelCenter;
 
+        /// <summary>The label to the right of the cursor.</summary>
         public UILabel LabelRight;
 
+        /// <summary>The screen-space position of the cursor relative to the paragraph, or <see cref="Location.NaN"/> if no cursor should be drawn.</summary>
         public Location CursorRenderOffset = Location.NaN;
     }
 
+    /// <summary>Data internal to a <see cref="UIInputParagraph"/> instance.</summary>
     public InternalData InputInternal = new();
 
-    public UIInputParagraph(UIStyling styling, UIStyling textStyling, UIStyling highlightStyling, UILayout layout) : base(styling, layout)
+    /// <summary>Constructs a new <see cref="UIInputParagraph"/>.</summary>
+    /// <param name="styling">The styling of the paragraph, used when drawing the cursor.</param>
+    /// <param name="textStyling">The styling for non-selected text.</param>
+    /// <param name="highlightStyling">The styling for selected, or highlighted, text.</param>
+    /// <param name="layout">The layout of this element.</param>
+    public UIInputParagraph(UIStyling styling, UIStyling textStyling, UIStyling highlightStyling, UILayout layout) : base(layout)
     {
+        Styling = styling;
         TextStyling = textStyling;
         HighlightStyling = highlightStyling;
         AddLabel(InputInternal.LabelLeft = new UILabel("", textStyling, new UILayout()));
@@ -79,23 +105,28 @@ public class UIInputParagraph : UIParagraph
         AddLabel(InputInternal.LabelRight = new UILabel("", textStyling, new UILayout()));
     }
 
+    /// <summary>Ensures the cursor positions lie within the current paragraph <see cref="Content"/>.</summary>
     public void ClampCursorPositions()
     {
         CursorStart = Math.Clamp(CursorStart, 0, Content.Length);
         CursorEnd = Math.Clamp(CursorEnd, 0, Content.Length);
     }
 
+    /// <summary>Sets both cursor positions to one index.</summary>
     public void SetCursorPosition(int cursorPosition)
     {
         CursorStart = CursorEnd = cursorPosition;
     }
 
+    /// <summary>Sets the raw paragraph text content.</summary>
+    /// <remarks>This operation does <b>not</b> call <see cref="UpdateRenderState"/>.</remarks>
     public void SetContent(string content)
     {
-        InputInternal.Content = content;
+        InputInternal.Content = content ?? "";
         ClampCursorPositions();
     }
 
+    /// <summary>Updates the text content of the three internal labels based on the cursor positions.</summary>
     public void UpdateLabelContent()
     {
         InputInternal.LabelLeft.Content = Content[..CursorLeft];
@@ -103,6 +134,7 @@ public class UIInputParagraph : UIParagraph
         InputInternal.LabelRight.Content = Content[CursorRight..];
     }
 
+    /// <summary>Prepares this paragraph for rendering.</summary>
     public void UpdateRenderState()
     {
         UpdateLabelContent();
@@ -110,10 +142,9 @@ public class UIInputParagraph : UIParagraph
         InputInternal.CursorRenderOffset = RenderCursor ? GetCursorRenderOffset() : Location.NaN;
     }
 
-    // TODO: update docs
-    /// <summary>Calculates a screen cursor offset given the current <see cref="TextChain"/>, or <see cref="Location.NaN"/> if none.</summary>
     // TODO: Account for formatting codes
     // TODO: out of range exception here somewhere
+    /// <summary>Returns the cursor's screen-space position relative to this element based on the cursor index position.</summary>
     public Location GetCursorRenderOffset()
     {
         double xOffset = 0;
@@ -150,6 +181,7 @@ public class UIInputParagraph : UIParagraph
         return Location.Zero;
     }
 
+    /// <inheritdoc/>
     public override void Render(double delta, UIStyle style)
     {
         base.Render(delta, style);
