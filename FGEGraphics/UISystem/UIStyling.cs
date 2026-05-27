b7600c4511a615ac7a084f8c9fed2d5e73dc416d
Skye Prince <spiralixp@gmail.com>
2026-05-26 22:40:34 -0700

    doc uistyling

--- FGEGraphics/UISystem/UIStyling.cs
+++ FGEGraphics/UISystem/UIStyling.cs
@@ -1,3 +1,11 @@
+//
+// This file is part of the Frenetic Game Engine, created by Frenetic LLC.
+// This code is Copyright (C) Frenetic LLC under the terms of a strict license.
+// See README.md or LICENSE.txt in the FreneticGameEngine source root for the contents of the license.
+// If neither of these are available, assume that neither you nor anyone other than the copyright holder
+// hold any right or permission to use this software until such time as the official license is identified.
+//
+
 using FGECore.ConsoleHelpers;
 using FGECore.MathHelpers;
 using FGEGraphics.GraphicsHelpers.FontSets;
@@ -10,29 +18,45 @@ using System.Threading.Tasks;
 
 namespace FGEGraphics.UISystem;
 
+/// <summary>Represents the styling logic of a <see cref="UIElement"/>.</summary>
 public record UIStyling
 {
+    /// <summary>
+    /// The element bound to this style.
+    /// <para>If present, calls to <see cref="UIStyleValue{T}.Get(UIElement)"/> will use this element rather than the one passed as an argument.</para>
+    /// </summary>
     public UIElement Element = null;
 
+    /// <summary>The color to fill an element's interior with.</summary>
     public UIStyleValue<Color4F> Fill = Color4F.Transparent;
 
+    /// <summary>The texture to draw on an element.</summary>
     public UIStyleValue<Texture> Texture = default;
 
+    /// <summary>The color to draw an element's outline with.</summary>
     public UIStyleValue<Color4F> Stroke = Color4F.Transparent;
 
+    /// <summary>The thickness to draw an element's outline with.</summary>
     public UIStyleValue<int> StrokeWeight = 0;
 
+    /// <summary>The distance between an element's outline and its interior content.</summary>
     public UIStyleValue<int> Padding = 0;
 
+    /// <summary>The size of the drop shadow on an element.</summary>
     public UIStyleValue<int> ShadowSize = 0;
 
+    /// <summary>The text font.</summary>
     public UIStyleValue<FontSet> TextFont = default;
 
+    /// <summary>The text styling effect.</summary>
     public UIStyleValue<Func<string, string>> TextStyling = default;
 
+    /// <summary>The base color effect for text.</summary>
     public UIStyleValue<string> TextBaseColor = TextStyle.Simple;
 
-    public UIStyle Get(UIElement element) {
+    /// <summary>Returns a new <see cref="UIStyle"/> using style values based on the given <paramref name="element"/>.</summary>
+    public UIStyle Get(UIElement element) 
+    {
         element = Element ?? element;
         return new()
         {
@@ -48,5 +72,6 @@ public record UIStyling
         };
     }
 
+    /// <summary>Returns this styling instance with <see cref="Element"/> set to <paramref name="element"/>.</summary>
     public UIStyling Bind(UIElement element) => this with { Element = element };
 }
